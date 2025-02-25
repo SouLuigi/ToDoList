ed7f1d17734ded91bbc045733e95c71992a3fb7c
souluigi <marcos2003mlr@gmail.com>
2025-02-25 09:44:45 -0300

    refactor

--- app/src/main/java/com/ex/todolist/MainActivity.kt
+++ app/src/main/java/com/ex/todolist/MainActivity.kt
@@ -11,10 +11,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity(), AddNewTask.AddTaskListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var taskAdapter: TaskAdapter
-    private val taskViewModel: TaskViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,34 +24,5 @@ class MainActivity : AppCompatActivity(), AddNewTask.AddTaskListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewTasks)
-        taskAdapter = TaskAdapter(
-            emptyList(),
-            onTaskClickListener = { task ->
-                Snackbar.make(recyclerView, "Tarefa clicada: ${task.title}", Snackbar.LENGTH_SHORT).show()
-            },
-            onCheckBoxClickListener = { task, isChecked ->
-                taskViewModel.toggleTaskCompletion(task)
-            }
-        )
-        recyclerView.adapter = taskAdapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        taskViewModel.tasks.observe(this) { tasks ->
-            taskAdapter.updateTasks(tasks)
-        }
-
-        val buttonNewTask: MaterialButton = findViewById(R.id.buttonNewTask)
-        buttonNewTask.setOnClickListener {
-            val dialog = AddNewTask()
-            dialog.listener = this
-            dialog.show(supportFragmentManager, "AddNewTask")
-        }
-    }
-
-    override fun onTaskAdded(task: Task) {
-        taskViewModel.addTask(task.title, task.date, task.category)
-        Snackbar.make(findViewById(R.id.main), "Tarefa adicionada!", Snackbar.LENGTH_SHORT).show()
     }
 }
